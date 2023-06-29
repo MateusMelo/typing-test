9a411b05984502e33bce0f707046bd22fe9f3f99
MateusMelo <mateusmelodasilva@gmail.com>
2023-06-28 23:41:40 -0300

    feat: wip apply styles from state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,45 @@
-import Image from 'next/image'
+'use client'
+
+import { KeyboardEvent, useState } from 'react';
 import styles from './page.module.css'
 
+interface Word {
+  word: string,
+  classes: Array<string>
+}
+
 export default function Home() {
+  const [words, setWords] = useState<Word[]>([
+    { word: "em", classes: ["word", "current"] },
+    { word: "termo", classes: ["word"] },
+    { word: "cada", classes: ["word"] },
+    { word: "nem", classes: ["word"] }
+  ])
+  // const [currentWord, setCurrentWord] = useState<string>(words[0])
+  // const [typedWord, setTypedWord] = useState<string>("")
+  // let pointer = 0;
+
+  function handleKeyUp(e: KeyboardEvent<HTMLInputElement>) {
+    // if (e.key === " " || currentWord === typedWord) {
+    //   setCurrentWord(words[++pointer]);
+    //   setTypedWord("");
+    // } else {
+    //   setTypedWord(e.key);
+    // }
+  }
+
+  function applyClasses(classes: Array<string>): string {
+    return classes.reduce((acc, curr) => `${acc} ${styles[curr]}`, "");
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div>
+        {words.map(({ word, classes }, i) => (
+          <span className={applyClasses(classes)} key={i}>{word}</span>
+        ))}</div>
+      <div>
+        <input onKeyUp={handleKeyUp} type="text" />
       </div>
     </main>
   )
